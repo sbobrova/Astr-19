ba22cf2029cf978fd8e38934b802f175f7528267
sbobrova <sbobrova@ucsc.edu>
2025-10-16 21:30:36 -0700

    Add files via upload

---
+++ journal-prompt-1.py
@@ -0,0 +1,11 @@
+def aboutmyself():
+	print(f"Hello! my name is Sage.")
+	print(f"My pronouns are She/Her.")
+	print(f"My favorite movie is American Psycho.")
+	print(f"My favorite food is either poke or sushi.")
+
+def main():
+	aboutmyself()
+
+if __name__=="__main__":
+	main()
